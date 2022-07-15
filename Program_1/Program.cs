7f09b70944cb3e197e8fd34d8894a045119c861f
Shchepetilnikov <kalugalina.gugl@gmail.com>
2022-07-15 20:50:32 +0300

    Added homework 6

---
+++ Program_1/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] UserArray (int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Input number " + (i+1));
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void FindPositiv (int[] array)
+{
+    int result = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) result ++;
+    }
+    Console.WriteLine("Value of positiv: " + result);
+}
+
+Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = UserArray(size);
+
+FindPositiv(array);
+
+
---
+++ Program_2/Program.cs
@@ -0,0 +1,31 @@
+﻿double[] Intersection(int k1, int b1, int k2, int b2)
+{
+    double[] array = new double[2];
+    array[0] = (double)(b2 - b1) / (k1 - k2);
+    array[1] = k1 * array[0] + b1;
+    return array;
+}
+
+void ShowArray(double[] array)
+{
+    for(int i = 0; i < 2; i++)
+        Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine("input k1 value: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input b1 value: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input k2 value: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input b2 value: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+double[] result = Intersection(k1, b1, k2, b2);
+
+ShowArray(result);
